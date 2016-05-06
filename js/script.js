70ee7f8e90bf07c652f95ecb76f602cd1235ac39
Indra Bijay <indrabijay007@gmail.com>
2016-05-05 22:40:30 -0700

    Add the logic to validate the feild using ng-messages

--- js/script.js
+++ js/script.js
@@ -1,51 +1,44 @@
-angular.module('myApp',[])
-    .controller('myCtrl',['$scope', function($scope){
+angular.module('myApp',['ngMessages'])
+    .controller('WaitStaffController',['$scope', function($scope){
 
-        $scope.tip_total=0;
-        $scope.meal_count=0;
-        $scope.average_meal_tip=0;
-        $scope.tiptotal=0;
-
-        $scope.submit=function()
-
-        {
-            var base_meal_price=Number($scope.base_meal_price);
-            var tax=($scope.tax_rate/100) + base_meal_price;
-            var subtotal=(tax + base_meal_price);
-            $scope.subtotal=(base_meal_price+ tax);
-            var tip=Number($scope.tip_percentage/100) * subtotal ;
-            $scope.tip=tip;
-            $scope.total=subtotal + tip;
-            $scope.tip_total=$scope.tip_total + tip;
-            $scope.meal_count=$scope.meal_count+1;
-            $scope.average_meal_tip=$scope.tip_total/$scope.meal_count;
-
+        $scope.TipTotal = 0;
+        $scope.MealCount = 0;
+        $scope.AverageMealPrice = 0;
+        $scope.TipTotal = 0;
+        $scope.submit = function() {
+            $scope.UserForm.$submitted = true;
+            var base_meal_price = Number($scope.BaseMealPrice);
+            var tax = ($scope.TaxRate/100) + base_meal_price;
+            var subtotal = (tax + base_meal_price);
+            $scope.SubTotal = (base_meal_price+ tax);
+            var tip = Number($scope.TipPercentage/100) * subtotal ;
+            $scope.Tip = tip;
+            $scope.Total = subtotal + tip;
+            $scope.TipTotal = $scope.tip_total + tip;
+            $scope.MealCount = $scope.meal_count+1;
+            $scope.AverageMealPrice = $scope.tip_total/$scope.meal_count;
         }
 
-        $scope.reset=function()
-        {
-            $scope.tip_total=undefined;
-            $scope.meal_count=undefined;
-            $scope.average_meal_tip=undefined;
-            $scope.tiptotal=undefined;
-            $scope.subtotal=undefined;
-            $scope.tip=undefined;
-            $scope.total=undefined;
-            $scope.base_meal_price=undefined;
-            $scope.tax_rate=undefined;
-            $scope.tip_percentage=undefined;
+        $scope.reset = function() {
+            $scope.TipTotal = null;
+            $scope.MealCount = null;
+            $scope.AverageMealPrice = null;
+            $scope.TipTotal = null;
+            $scope.SubTotal = null;
+            $scope.Tip = null;
+            $scope.Total = null;
+            $scope.BaseMealPrice = null;
+            $scope.TaxRate = null;
+            $scope.TipPercentage = null;
+            $scope.UserForm.$submitted = false;
+        }
+
+        $scope.cancel = function() {
+            $scope.BaseMealPrice = null;
+            $scope.TaxRate = null;
+            $scope.TipPercentage = null;
+            $scope.UserForm.$submitted = false;
 
 
         }
-
-        $scope.cancel=function()
-        {
-            $scope.base_meal_price=undefined;
-            $scope.tax_rate=undefined;
-            $scope.tip_percentage=undefined;
-
-        }
-
-
-
     }]);
